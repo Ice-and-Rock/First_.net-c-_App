18c7fb2162a34119120d929aea372c8c7a98a3bb
nick v <nvalente.electrics@gmail.com>
2023-12-10 21:18:30 +0100

    changed initial console.log message

---
+++ First_project/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, what is your name?");
+var name = Console.Readline()
+var currentDate = DateTime.now() 
+Console.WriteLine ( $"{Environment.NewLine} Hello {name} on {currentDate:d} ) ;
+Console.Write($"{Environment.NewLine}Pressanykeytoexit...");
+Console. ReadKey (true);
